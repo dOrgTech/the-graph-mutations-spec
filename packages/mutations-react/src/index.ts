86b664243bc0bfd75b93e403891dbf3cd30fb9d4
dOrgJelli <jelli@dorg.tech>
2020-01-11 19:05:57 -0500

    2 space tabs

--- packages/mutations-react/src/index.ts
+++ packages/mutations-react/src/index.ts
@@ -3,40 +3,42 @@ import { useMutation } from '@apollo/react-hooks';
 import { BehaviorSubject } from 'rxjs';
 import { MutationState } from '@graphprotocol/mutations-ts'
 
-function useObservable<CustomMutationStateType>(observable: BehaviorSubject<CustomMutationStateType>,
-    setter: React.Dispatch<React.SetStateAction<CustomMutationStateType>>) {
-         
-    useEffect(() => {
-        let subscription = observable.subscribe(result => {
-            if (result) setter(result)
-        })
-        return () => subscription.unsubscribe();
-    }, [observable, setter])
+function useObservable<CustomMutationStateType>(
+  observable: BehaviorSubject<CustomMutationStateType>,
+  setter: React.Dispatch<React.SetStateAction<CustomMutationStateType>>
+) {
+  useEffect(() => {
+    let subscription = observable.subscribe(result => {
+      if (result) setter(result)
+    })
+    return () => subscription.unsubscribe();
+  }, [observable, setter])
 }
 
-export function useMutationAndSubscribe<CustomMutationStateType>(mutation: any,
+export function useMutationAndSubscribe<CustomMutationStateType>(
+    mutation: any,
     mutationOptions: any,
-    CustomMutationState: new (observable: BehaviorSubject<CustomMutationStateType>) => CustomMutationStateType) {
+    CustomMutationState: new (observable: BehaviorSubject<CustomMutationStateType>) => CustomMutationStateType
+) {
 
-    const [subscriptionData, setSubscriptionData] = useState({} as CustomMutationStateType)
+  const [subscriptionData, setSubscriptionData] = useState({} as CustomMutationStateType)
 
-    const [observable] = useState(new BehaviorSubject({} as CustomMutationStateType))
+  const [observable] = useState(new BehaviorSubject({} as CustomMutationStateType))
 
-    mutationOptions.context = {
-        ...mutationOptions.context,
-        mutationState: new CustomMutationState(observable)
-    }
+  mutationOptions.context = {
+    ...mutationOptions.context,
+    mutationState: new CustomMutationState(observable)
+  }
 
-    const [executeMutation, loading] = useMutation(mutation, mutationOptions)
+  const [executeMutation, loading] = useMutation(mutation, mutationOptions)
 
-    useObservable<CustomMutationStateType>(observable, setSubscriptionData);
+  useObservable<CustomMutationStateType>(observable, setSubscriptionData);
 
-    return {
-        executeMutation: () => {
-            executeMutation();
-        },
-        loadingMutation: loading,
-        subscriptionData
-    }
+  return {
+    executeMutation: () => {
+      executeMutation();
+    },
+    loadingMutation: loading,
+    subscriptionData
+  }
 }
-

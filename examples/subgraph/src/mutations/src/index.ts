2efee4c55929faa8ae267ef2416b3148a4604333
dOrgJelli <jelli@dorg.tech>
2020-01-12 00:58:37 -0500

    adjusting resolvers implementation

--- examples/subgraph/src/mutations/src/index.ts
+++ examples/subgraph/src/mutations/src/index.ts
@@ -25,15 +25,16 @@ async function queryUserGravatar(context: any) {
   )
 }
 
-async function sendTx(tx: any, id: string, msg: string, progress: number, context: any) {
+// TODO: remove ID at this level, pass it in using the uuid
+async function sendTx(tx: any, msg: string, progress: number, context: any) {
   const state: MutationState = context.state;
   try {
-    state.startTransaction({id, title: msg, payload: {}})
+    state.startTransaction({ title: msg, payload: {} })
     tx = await tx
-    state.confirmTransaction(id, progress, tx)
     await tx.wait()
+    state.confirmTransaction(progress, tx)
   } catch (error) {
-    state.addError(id, error)
+    state.addError(error)
     throw new Error(`Failed while sending "${msg}"`)
   }
 }
@@ -53,27 +54,26 @@ async function getGravityContract(context: any) {
 async function createGravatar(_root: any, {options}: any, context: any) {
   const { displayName, imageUrl } = options
   const gravity = await getGravityContract(context)
-  const tx = await gravity.createGravatar(displayName, imageUrl)
-  //await sendTx(tx, "Creating Gravatar", context)
+  const tx = gravity.createGravatar(displayName, imageUrl)
+  await sendTx(tx, "Creating Gravatar", 0.9, context)
   return await queryUserGravatar(context)
 }
 
 async function updateGravatarName(_root: any, {displayName}: any, context: any) {
   const gravity = await getGravityContract(context)
   const tx = gravity.updateGravatarName(displayName)
-  const randId = Math.floor(Math.random()*(10000000000+1)).toString();
-  await sendTx(tx, randId ,"Updating Gravatar Name", 0.25, context)
+  await sendTx(tx, "Updating Gravatar Name", 0.9, context)
   return await queryUserGravatar(context)
 }
 
 async function updateGravatarImage(_root: any, {imageUrl}: any, context: any) {
   const gravity = await getGravityContract(context)
   const tx = gravity.updateGravatarImage(imageUrl)
+  await sendTx(tx, "Updating Gravatar Image", 0.9, context)
 
   // Example of custom data within the state
   context.graph.state.addData("imageUrl", imageUrl)
 
-  //await sendTx(tx, "Updating Gravatar Image", context)
   return await queryUserGravatar(context)
 }
 

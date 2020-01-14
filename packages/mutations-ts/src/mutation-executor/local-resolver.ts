52345a81bf2e85c8f1efe5637d04c4811f74a1bd
Nestor <nestor09amesty@gmail.com>
2020-01-14 20:28:01 +0100

    Client directive verification in local mutation resolver executor

--- packages/mutations-ts/src/mutation-executor/local-resolver.ts
+++ packages/mutations-ts/src/mutation-executor/local-resolver.ts
@@ -2,6 +2,7 @@ import { execute, makePromise } from 'apollo-link'
 import { withClientState } from 'apollo-link-state'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { MutationQuery, MutationResult, Resolvers } from '../types'
+import { OperationDefinitionNode } from 'graphql'
 
 export default async (mutationQuery: MutationQuery, resolvers: Resolvers): Promise<MutationResult> => {
   // TODO: note that this is being thrown away each time... desired?
@@ -10,6 +11,14 @@ export default async (mutationQuery: MutationQuery, resolvers: Resolvers): Promi
     cache,
     resolvers
   })
+
+  if(mutationQuery.query.definitions[0].kind === "OperationDefinition"){
+    const directives = mutationQuery.query.definitions[0].selectionSet.selections[0].directives;
+    if(!directives || !directives.find(directive => directive.name.value === "client")){
+      throw new Error(`Mutation '${mutationQuery.operationName}' is missing client directive`)
+    }
+  }
+  
   let result = await makePromise(
     execute(link, {
       query: mutationQuery.query,

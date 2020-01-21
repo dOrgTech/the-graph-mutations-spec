913335d0263b5c179da287ba6df6a187eb24421a
dOrgJelli <jelli@dorg.tech>
2020-01-21 15:50:20 -0500

    formatting changes

--- packages/mutations-apollo-react/src/index.ts
+++ packages/mutations-apollo-react/src/index.ts
@@ -12,14 +12,14 @@ import { CoreState } from '@graphprotocol/mutations-ts'
 import { MutationTupleWithState } from './types'
 import { useObservable } from './utils'
 
-export function useMutation<
+export const useMutation = <
   TState = CoreState,
   TData = any,
   TVariables = OperationVariables
 >(
   mutation: DocumentNode,
   mutationOptions: MutationHookOptions<TData, TVariables>
-): MutationTupleWithState<TState, TData, TVariables> {
+): MutationTupleWithState<TState, TData, TVariables> => {
 
   const [state, setState] = useState({} as TState)
   const [observable] = useState(new BehaviorSubject({} as TState))
